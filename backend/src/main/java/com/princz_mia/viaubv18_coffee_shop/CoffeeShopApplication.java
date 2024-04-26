58c3a99f12d0e0744bbff0d2543285d1cd7740cd
Princz Mia <princz.mia@gmail.com>
2024-04-26 20:17:42 +0200

    Add 'EnableJpaAuditing' and 'EnableAsync' annotations

--- backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/CoffeeShopApplication.java
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/CoffeeShopApplication.java
@@ -2,8 +2,12 @@ package com.princz_mia.viaubv18_coffee_shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableAsync
 public class CoffeeShopApplication {
 
 	public static void main(String[] args) {
