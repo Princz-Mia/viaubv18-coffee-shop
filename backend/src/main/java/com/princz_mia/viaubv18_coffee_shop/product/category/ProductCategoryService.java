f07713789c337e239e59f482e151cd4145f78e5f
Princz Mia <princz.mia@gmail.com>
2024-04-26 20:30:33 +0200

    Add 'ProductCategory' feature skeleton Product

--- backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/product/category/ProductCategoryService.java
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/product/category/ProductCategoryService.java
@@ -1,7 +1,9 @@
 package com.princz_mia.viaubv18_coffee_shop.product.category;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@Service
 @Slf4j
 public class ProductCategoryService {
 }
