7e653adbb132705689779dae3de3adea5bd23526
Princz Mia <princz.mia@gmail.com>
2024-04-28 21:28:59 +0200

    Add rudimentary implementation of Email Sender Service

---
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/email/EmailService.java
@@ -0,0 +1,61 @@
+package com.princz_mia.viaubv18_coffee_shop.email;
+
+import com.princz_mia.viaubv18_coffee_shop.exception.AppException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import static com.princz_mia.viaubv18_coffee_shop.email.EmailUtils.getEmailMessage;
+import static com.princz_mia.viaubv18_coffee_shop.email.EmailUtils.getResetPasswordMessage;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EmailService {
+
+    private static final String NEW_USER_ACCOUNT_VERIFICATION = "New User Account Verification";
+    private static final String PASSWORD_RESET_REQUEST = "Reset Password Request";
+    private final JavaMailSender sender;
+
+    @Value("http://localhost:3000")
+    private String host;
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    // TODO: Implement EmailBuilder function class to get rid of code duplication
+
+    @Async
+    public void sendNewAccountEmail(String name, String toEmail, String key) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setSubject(NEW_USER_ACCOUNT_VERIFICATION);
+            message.setFrom(fromEmail);
+            message.setTo(toEmail);
+            message.setText(getEmailMessage(name, host, key));
+            sender.send(message);
+        } catch (Exception e) {
+            // Should not throw Exception. Inform the User about the error and tell them to later try it again.
+            throw new AppException("Unable to send email, cause: " + e.getMessage(), HttpStatus.BAD_GATEWAY);
+        }
+    }
+
+    @Async
+    public void sendPasswordResetEmail(String name, String toEmail, String token) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setSubject(PASSWORD_RESET_REQUEST);
+            message.setFrom(fromEmail);
+            message.setTo(toEmail);
+            message.setText(getResetPasswordMessage(name, host, token));
+            sender.send(message);
+        } catch (Exception e) {
+            // Should not throw Exception. Inform the User about the error and tell them to later try it again.
+            throw new AppException("Unable to send email", HttpStatus.BAD_GATEWAY);
+        }
+    }
+}
---
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/email/EmailUtils.java
@@ -0,0 +1,24 @@
+package com.princz_mia.viaubv18_coffee_shop.email;
+
+public class EmailUtils {
+
+    // TODO: Refactor with StringBuilder
+
+    public static String getEmailMessage(String name, String host, String key) {
+        return "Hello " + name + ",\n\nYour new account has been created. Please click on the link below to verify your account.\n\n" +
+                getVerificationUrl(host, key) + "\n\nThe Support Team";
+    }
+
+    public static String getResetPasswordMessage(String name, String host, String key) {
+        return "Hello " + name + ",\n\nYou have requested password restoration. Please click on the link below to reset your password.\n\n" +
+                getResetPasswordUrl(host, key) + "\n\nThe Support Team";
+    }
+
+    public static String getVerificationUrl(String host, String key) {
+        return host + "/verify/account?key=" + key;
+    }
+
+    public static String getResetPasswordUrl(String host, String key) {
+        return host + "/verify/password?key=" + key;
+    }
+}
