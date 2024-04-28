98d27cfa059d545a9b2dc898e9d4dba1126ef43a
Princz Mia <princz.mia@gmail.com>
2024-04-28 21:35:32 +0200

    Add UserDto to handle User entity data easier

---
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/user/UserDto.java
@@ -0,0 +1,32 @@
+package com.princz_mia.viaubv18_coffee_shop.user;
+
+import com.princz_mia.viaubv18_coffee_shop.address.Address;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserDto {
+    private Long id;
+    private Long createdBy;
+    private Long updatedBy;
+    private String createdAt;
+    private String updatedAt;
+    private Address address;
+    private String role;
+    private String authorities;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private Integer loginAttempts;
+    private String lastLogin;
+    private boolean isAccountNonLocked;
+    private boolean isEnabled;
+    private String token;
+}
