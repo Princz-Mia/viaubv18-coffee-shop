a46f6d273e40bf7dc8d4408b3e68eab31569fe12
Princz Mia <princz.mia@gmail.com>
2024-04-29 21:01:17 +0200

    Add ProductController with mapping for /products and /product/{name}

--- backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/product/ProductController.java
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/product/ProductController.java
@@ -2,12 +2,30 @@ package com.princz_mia.viaubv18_coffee_shop.product;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/product")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 @Slf4j
 public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/products")
+    public ResponseEntity<ProductPageResponse> getPageOfProducts(
+            @RequestParam(value = "pageNumber", defaultValue = "0", required = false) int pageNumber,
+            @RequestParam(value = "pageSize", defaultValue = "8", required = false) int pageSize
+    ) {
+        ProductPageResponse productPage = productService.getPageOfProducts(pageNumber, pageSize);
+        return ResponseEntity.ok(productPage);
+    }
+
+
+    @GetMapping("/product/{name}")
+    public ResponseEntity<Product> getProductByName(@PathVariable(value = "name") String name) {
+        Product product = productService.getProductByName(name);
+        return ResponseEntity.ok(product);
+    }
 }
