365e21bf43f42cc8d35fafa89812cede85c04183
Princz Mia <princz.mia@gmail.com>
2024-04-30 21:22:17 +0200

    Add function to create new ShoppingCart entity connected to User entity in param

--- backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/shopping_cart/ShoppingCartService.java
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/shopping_cart/ShoppingCartService.java
@@ -1,9 +1,24 @@
 package com.princz_mia.viaubv18_coffee_shop.shopping_cart;
 
+import com.princz_mia.viaubv18_coffee_shop.exception.AppException;
+import com.princz_mia.viaubv18_coffee_shop.user.User;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class ShoppingCartService {
+
+    private final ShoppingCartRepository shoppingCartRepository;
+
+    public void createShoppingCartForVerifiedUser(User user) {
+        if (!user.isEnabled())
+            throw new AppException("User is not verified yet. Could not create ShoppingCart", HttpStatus.BAD_REQUEST);
+
+        ShoppingCart shoppingCart = new ShoppingCart(user);
+        shoppingCartRepository.save(shoppingCart);
+    }
 }
