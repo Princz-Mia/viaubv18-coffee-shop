f46f2f8ba801cefee5cb5ffedcf56eb1f7bebaaa
Princz Mia <princz.mia@gmail.com>
2024-05-02 21:32:49 +0200

    Add findByContainingSearchTermInName with custom query annotation and findAllByNameContainsIgnoreCase

--- backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/product/ProductRepository.java
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/product/ProductRepository.java
@@ -1,11 +1,22 @@
 package com.princz_mia.viaubv18_coffee_shop.product;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
     Optional<Product> findByNameIgnoreCase(String name);
+
+    Page<Product> findAllByNameContainsIgnoreCase(String searchTerm, Pageable pageable);
+
+    @Query(value = "SELECT p FROM Product p WHERE LOWER(p.name) LIKE %:searchTerm%")
+    List<Product> findByContainingSearchTermInName(@Param("searchTerm") String searchTerm);
 }
