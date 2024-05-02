2891acbf05f0020c0186f4f7ea0f82e6db022f02
Princz Mia <princz.mia@gmail.com>
2024-05-02 21:36:02 +0200

    Add GET mapping to get ShoppingCartItem entities in specific ShoppingCart Add POST mapping to response to add and remove API calls

--- backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/shopping_cart/item/ShoppingCartItemController.java
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/shopping_cart/item/ShoppingCartItemController.java
@@ -1,13 +1,41 @@
 package com.princz_mia.viaubv18_coffee_shop.shopping_cart.item;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/cart/item")
+@RequestMapping("/api/v1/cartItem")
 @RequiredArgsConstructor
 @Slf4j
 public class ShoppingCartItemController {
+
+    private final ShoppingCartItemService shoppingCartItemService;
+
+    @GetMapping(path = "/getByCartId/{cartId}")
+    public ResponseEntity<List<ShoppingCartItem>> getShoppingCartItemsByCartId(@PathVariable(value = "cartId") Long cartId) {
+        List<ShoppingCartItem> shoppingCartItems = shoppingCartItemService.getShoppingCartItemsByCartId(cartId);
+        return ResponseEntity.ok(shoppingCartItems);
+    }
+
+    @PostMapping(path = "/add")
+    public ResponseEntity<ShoppingCartItem> addShoppingCartItemToCart(@RequestBody @Valid ShoppingCartItemRequest shoppingCartItemRequest) {
+        ShoppingCartItem savedShoppingCartItem = shoppingCartItemService.addShoppingCartItemToCart(shoppingCartItemRequest);
+        return ResponseEntity.ok(savedShoppingCartItem);
+    }
+
+    @PostMapping(path = "/remove")
+    public ResponseEntity<?> removeShoppingCartItemToCart(@RequestBody @Valid ShoppingCartItemRequest shoppingCartItemRequest) {
+        boolean isRemovalFailed = shoppingCartItemService.removeShoppingCartItemToCart(shoppingCartItemRequest);
+
+        if (isRemovalFailed)
+            return new ResponseEntity<>("Failed removal.", HttpStatus.NOT_MODIFIED);
+
+        return new ResponseEntity<>("Successful removal.", HttpStatus.ACCEPTED);
+    }
 }
