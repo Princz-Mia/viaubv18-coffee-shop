f20410e2c6fb83cde2b0c6717ccdbbc01877e065
Princz Mia <princz.mia@gmail.com>
2024-05-02 21:41:46 +0200

    Add features to ShoppingCartService such as getShoppingCartById and getShoppingCartByUserId

--- backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/shopping_cart/ShoppingCartService.java
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/shopping_cart/ShoppingCartService.java
@@ -21,4 +21,20 @@ public class ShoppingCartService {
         ShoppingCart shoppingCart = new ShoppingCart(user);
         shoppingCartRepository.save(shoppingCart);
     }
+
+    public ShoppingCart getShoppingCartById(Long id) {
+        if (id == null || id < 0)
+            throw new AppException("Cart Id is not valid", HttpStatus.BAD_REQUEST);
+
+        var shoppingCart = shoppingCartRepository.findById(id);
+        return shoppingCart.orElseThrow(() -> new AppException("Cart is not found with matching Cart Id", HttpStatus.NOT_FOUND));
+    }
+
+    public ShoppingCart getShoppingCartByUserId(Long userId) {
+        if (userId == null || userId < 0)
+            throw new AppException("User Id is not valid", HttpStatus.BAD_REQUEST);
+
+        var shoppingCart = shoppingCartRepository.findByUserId(userId);
+        return shoppingCart.orElseThrow(() -> new AppException("Cart is not found with matching User Id", HttpStatus.NOT_FOUND));
+    }
 }
