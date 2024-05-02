71a69704267d9149682dc5ed167723c104295aac
Princz Mia <princz.mia@gmail.com>
2024-05-02 21:40:43 +0200

    Add GET mapping (/getById/{id}; /getByUserId/{userId}) in ShoppingCartController class

--- backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/shopping_cart/ShoppingCartController.java
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/shopping_cart/ShoppingCartController.java
@@ -2,8 +2,8 @@ package com.princz_mia.viaubv18_coffee_shop.shopping_cart;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/cart")
@@ -12,4 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class ShoppingCartController {
 
     private final ShoppingCartService shoppingCartService;
+
+
+    @GetMapping(path = "/getById/{id}")
+    public ResponseEntity<ShoppingCart> getShoppingCartById(@PathVariable(value = "id") Long id) {
+        ShoppingCart shoppingCartByUserId = shoppingCartService.getShoppingCartById(id);
+        return ResponseEntity.ok(shoppingCartByUserId);
+    }
+
+    @GetMapping(path = "/getByUserId/{userId}")
+    public ResponseEntity<ShoppingCart> getShoppingCartByUserId(@PathVariable(value = "userId") Long userId) {
+        ShoppingCart shoppingCartByUserId = shoppingCartService.getShoppingCartByUserId(userId);
+        return ResponseEntity.ok(shoppingCartByUserId);
+    }
 }
