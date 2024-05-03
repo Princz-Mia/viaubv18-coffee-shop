a9b7b94ce4cacd280900ecd4f4b6f6ab85d523e9
Princz Mia <princz.mia@gmail.com>
2024-05-03 21:13:17 +0200

    Add findByPropertiesIgnoreCase function with custom Query annotation

--- backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/address/AddressRepository.java
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/address/AddressRepository.java
@@ -1,8 +1,24 @@
 package com.princz_mia.viaubv18_coffee_shop.address;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
+
+    @Query("SELECT a FROM Address a " +
+            "WHERE a.country.id = ?1" +
+            " and LOWER(a.region) = LOWER(?2)" +
+            " and LOWER(a.postalCode) = LOWER(?3)" +
+            " and LOWER(a.city) = LOWER(?4)" +
+            " and LOWER(a.addressLine1) = LOWER(?5)" +
+            " and LOWER(a.addressLine2) = LOWER(?6)" +
+            " and LOWER(a.streetNumber) = LOWER(?7)" +
+            " and LOWER(a.unitNumber) = LOWER(?8)"
+    )
+    Optional<Address> findByPropertiesIgnoreCase(Long countryId, String region, String postalCode, String city, String addressLine1, String addressLine2, String streetNumber, String unitNumber);
+
 }
