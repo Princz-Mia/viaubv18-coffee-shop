fce5156e91e0ae9158a16512ef29a8a5ee70a262
Princz Mia <princz.mia@gmail.com>
2024-05-03 21:09:55 +0200

    Add 'AddressController' with post mapping for Address entity creation

--- backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/address/AddressController.java
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/address/AddressController.java
@@ -1,13 +1,22 @@
 package com.princz_mia.viaubv18_coffee_shop.address;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/address")
 @RequiredArgsConstructor
 @Slf4j
 public class AddressController {
+
+    private final AddressService addressService;
+
+    @PostMapping("/createAddress")
+    public ResponseEntity<Address> createAddress(@RequestBody @Valid AddressRequest addressRequest) {
+        Address address = addressService.createAddress(addressRequest);
+        return ResponseEntity.ok(address);
+    }
 }
