79d739b67ed83f58cc10561996b2ff8aee60a43b
Princz Mia <princz.mia@gmail.com>
2024-05-03 21:17:29 +0200

    Add 'ShippingMethodController' with GET mapping to query all 'ShippingMethod' entity

--- backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/shipping_method/ShippingMethodController.java
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/shipping_method/ShippingMethodController.java
@@ -2,12 +2,24 @@ package com.princz_mia.viaubv18_coffee_shop.shipping_method;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/v1/shipping_method")
+@RequestMapping("/api/v1/shippingMethod")
 @RequiredArgsConstructor
 @Slf4j
 public class ShippingMethodController {
+
+    private final ShippingMethodService shippingMethodService;
+
+    @GetMapping(path = "/all")
+    public ResponseEntity<List<ShippingMethod>> getShippingMethods() {
+        List<ShippingMethod> shippingMethods = shippingMethodService.getShippingMethods();
+        return ResponseEntity.ok(shippingMethods);
+    }
 }
