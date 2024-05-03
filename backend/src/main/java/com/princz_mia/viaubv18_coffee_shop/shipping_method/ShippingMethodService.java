a4d7f5f8028b620dc074dc879a648a3563d03168
Princz Mia <princz.mia@gmail.com>
2024-05-03 21:18:21 +0200

    Add 'ShippingMethodService' with getShippingMethods function that returns all 'ShippingMethod' entities from database

--- backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/shipping_method/ShippingMethodService.java
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/shipping_method/ShippingMethodService.java
@@ -1,9 +1,19 @@
 package com.princz_mia.viaubv18_coffee_shop.shipping_method;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class ShippingMethodService {
+
+    private final ShippingMethodRepository shippingMethodRepository;
+
+    public List<ShippingMethod> getShippingMethods() {
+        return shippingMethodRepository.findAll();
+    }
 }
