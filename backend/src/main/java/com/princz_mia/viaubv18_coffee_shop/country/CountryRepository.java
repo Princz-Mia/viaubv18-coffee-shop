e236f0880788c05ac8cdd81763b2c27ea4f1169c
Princz Mia <princz.mia@gmail.com>
2024-05-03 21:13:41 +0200

    Add findByNameIgnoreCase query function

--- backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/country/CountryRepository.java
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/country/CountryRepository.java
@@ -3,6 +3,9 @@ package com.princz_mia.viaubv18_coffee_shop.country;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CountryRepository extends JpaRepository<Country, Long> {
+    Optional<Country> findByNameIgnoreCase(String countryName);
 }
