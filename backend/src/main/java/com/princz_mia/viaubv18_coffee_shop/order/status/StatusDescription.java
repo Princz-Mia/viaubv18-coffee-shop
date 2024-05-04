f71e4a1ef333f404be9a20824670b6b5877ab34d
Princz Mia <princz.mia@gmail.com>
2024-05-04 20:00:24 +0200

    Add 'StatusDescription' class with constant String properties

---
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/order/status/StatusDescription.java
@@ -0,0 +1,13 @@
+package com.princz_mia.viaubv18_coffee_shop.order.status;
+
+public class StatusDescription {
+    public static final String PENDING_DESCRIPTION = "Your order is pending processing. Please wait for further updates.";
+    public static final String AWAITING_PAYMENT_DESCRIPTION = "Your order is awaiting payment. Please complete the payment process to proceed.";
+    public static final String PAYMENT_FULFILLED_DESCRIPTION = "Payment for your order has been successfully processed.";
+    public static final String AWAITING_SHIPMENT_DESCRIPTION = "Your order is awaiting shipment. It will be shipped soon.";
+    public static final String PARTIALLY_SHIPPED_DESCRIPTION = "Your order has been partially shipped. Some items have been shipped, while others are pending.";
+    public static final String SHIPPED_DESCRIPTION = "Your order has been shipped. You should receive it shortly.";
+    public static final String CANCELLED_DESCRIPTION = "Your order has been cancelled. If you have any questions, please contact customer support.";
+    public static final String REFUNDED_DESCRIPTION = "Your order has been refunded. You will receive a refund for the cancelled items.";
+}
+
