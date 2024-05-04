6498ceca8f28c31a82e92adc5e86193023e21381
Princz Mia <princz.mia@gmail.com>
2024-05-04 20:04:41 +0200

    Change RequestMapping path value Add POST mapping for path value /create to 'createShopOrder' function Add GET mapping for path value /pending/{userId} to 'getPendingShopOrderByUserId' function

--- backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/order/ShopOrderController.java
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/order/ShopOrderController.java
@@ -1,13 +1,28 @@
 package com.princz_mia.viaubv18_coffee_shop.order;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/order")
+@RequestMapping("/api/v1/shopOrder")
 @RequiredArgsConstructor
 @Slf4j
 public class ShopOrderController {
+
+    private final ShopOrderService shopOrderService;
+
+    @PostMapping(path = "/create")
+    public ResponseEntity<ShopOrder> createShopOrder(@RequestBody @Valid ShopOrderRequest shopOrderRequest) {
+        ShopOrder shopOrder = shopOrderService.createShopOrder(shopOrderRequest);
+        return ResponseEntity.ok(shopOrder);
+    }
+
+    @GetMapping(path = "/pending/{userId}")
+    public ResponseEntity<ShopOrder> getPendingShopOrderByUserId(@PathVariable(value = "userId") Long userId) {
+        ShopOrder shopOrder = shopOrderService.getPendingShopOrderByUserId(userId);
+        return ResponseEntity.ok(shopOrder);
+    }
 }
