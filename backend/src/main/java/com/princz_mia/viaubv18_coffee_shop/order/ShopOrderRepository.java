3ec74c61884b86d17af05ffcb97694a0546b82f6
Princz Mia <princz.mia@gmail.com>
2024-05-04 20:06:40 +0200

    Add 'findByCustomerIdAndOrderStatusIgnoreCase' query function with custom query annotation

--- backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/order/ShopOrderRepository.java
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/order/ShopOrderRepository.java
@@ -5,6 +5,7 @@ import com.princz_mia.viaubv18_coffee_shop.order.status.OrderStatus;
 import com.princz_mia.viaubv18_coffee_shop.shipping_method.ShippingMethod;
 import com.princz_mia.viaubv18_coffee_shop.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Timestamp;
@@ -18,4 +19,7 @@ public interface ShopOrderRepository extends JpaRepository<ShopOrder, Long> {
     Optional<List<ShopOrder>> findByShippingMethod(ShippingMethod shippingMethod);
     Optional<List<ShopOrder>> findByOrderStatus(OrderStatus orderStatus);
     Optional<List<ShopOrder>> findByOrderDate(Timestamp orderDate);
+
+    @Query("SELECT s FROM ShopOrder s WHERE s.customer.id = ?1 and LOWER(s.orderStatus.name) = LOWER(?2)")
+    Optional<ShopOrder> findByCustomerIdAndOrderStatusIgnoreCase(Long userId, String name);
 }
