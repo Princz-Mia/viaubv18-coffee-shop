070a3d57f67f1e8bfa013b8a4f4ffa8de50e704a
Princz Mia <princz.mia@gmail.com>
2024-05-04 19:57:15 +0200

    Add 'findAllByOrderId' query function

--- backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/order/item/OrderItemRepository.java
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/order/item/OrderItemRepository.java
@@ -3,6 +3,10 @@ package com.princz_mia.viaubv18_coffee_shop.order.item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+    Optional<List<OrderItem>> findAllByOrderId(Long orderId);
 }
