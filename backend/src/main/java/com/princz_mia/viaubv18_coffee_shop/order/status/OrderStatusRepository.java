235fd53dfdeef09dab1e03ae45cea5c5d1a9bd17
Princz Mia <princz.mia@gmail.com>
2024-05-04 19:59:03 +0200

    Add 'findByNameIgnoreCase' query function

--- backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/order/status/OrderStatusRepository.java
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/order/status/OrderStatusRepository.java
@@ -5,4 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderStatusRepository extends JpaRepository<OrderStatus, Long> {
+    OrderStatus findByNameIgnoreCase(String name);
 }
