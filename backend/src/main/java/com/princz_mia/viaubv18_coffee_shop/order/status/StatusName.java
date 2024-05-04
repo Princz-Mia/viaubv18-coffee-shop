4c0dee74b2b7b1e960caf79544028269eb540055
Princz Mia <princz.mia@gmail.com>
2024-05-04 20:01:57 +0200

    Add 'StatusName' enum class to handle OrderStatus names and descriptions as its values

---
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/order/status/StatusName.java
@@ -0,0 +1,20 @@
+package com.princz_mia.viaubv18_coffee_shop.order.status;
+
+import static com.princz_mia.viaubv18_coffee_shop.order.status.StatusDescription.*;
+
+public enum StatusName {
+    PENDING(PENDING_DESCRIPTION),
+    AWAITING_PAYMENT(AWAITING_PAYMENT_DESCRIPTION),
+    PAYMENT_FULFILLED(PAYMENT_FULFILLED_DESCRIPTION),
+    AWAITING_SHIPMENT(AWAITING_SHIPMENT_DESCRIPTION),
+    PARTIALLY_SHIPPED(PARTIALLY_SHIPPED_DESCRIPTION),
+    SHIPPED(SHIPPED_DESCRIPTION),
+    CANCELLED(CANCELLED_DESCRIPTION),
+    REFUNDED(REFUNDED_DESCRIPTION);
+
+    private final String value;
+
+    StatusName(String value) { this.value = value; }
+
+    public String getValue() { return this.value; }
+}
