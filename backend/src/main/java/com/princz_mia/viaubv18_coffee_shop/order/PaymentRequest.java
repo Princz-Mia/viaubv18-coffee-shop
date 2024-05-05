cc7527160922c1a7cee71bab7bd3e2a379321c2d
Princz Mia <princz.mia@gmail.com>
2024-05-05 21:26:41 +0200

    Add PaymentRequest model for handling started PayPal transactions from frontend
    - Created PaymentRequest class with fields for shop order ID and payment ID
    - Added validation annotations to ensure non-null and non-empty values
    - Ignored unknown properties during JSON deserialization

---
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/order/PaymentRequest.java
@@ -0,0 +1,17 @@
+package com.princz_mia.viaubv18_coffee_shop.order;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class PaymentRequest {
+    @NotNull(message = "Shop Order Id cannot be null")
+    private Long shopOrderId;
+    @NotEmpty(message = "Payment Id cannot be null or empty")
+    private String paymentId;
+}
