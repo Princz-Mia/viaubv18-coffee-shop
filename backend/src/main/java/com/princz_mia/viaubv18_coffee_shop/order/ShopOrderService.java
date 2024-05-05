0598005024833939fdef5b386099cfad938cb17c
Princz Mia <princz.mia@gmail.com>
2024-05-05 21:23:11 +0200

    Enhance ShopOrderService with payment processing and additional methods
    - Added method for processing payment transactions
    - Added method for checking if a user has a pending shop order
    - Added method for retrieving shop orders by user ID
    - Updated createShopOrder method to include a null payment ID

--- backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/order/ShopOrderService.java
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/order/ShopOrderService.java
@@ -12,6 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -46,6 +48,7 @@ public class ShopOrderService {
                 .firstName(shopOrderRequest.getFirstName())
                 .lastName(shopOrderRequest.getLastName())
                 .phoneNumber(shopOrderRequest.getPhoneNumber())
+                .paymentId(null)
                 .build();
 
         return shopOrderRepository.save(shopOrder);
@@ -63,5 +66,37 @@ public class ShopOrderService {
         //shopOrder.setOrderStatus(status);
 
         return shopOrderRepository.save(shopOrder);
+    }
+
+    public ShopOrder processPayTransaction(PaymentRequest paymentRequest) {
+        var shopOrder = shopOrderRepository.findById(paymentRequest.getShopOrderId())
+                .orElseThrow(() -> new AppException("Shop Order is not found with matching User Id and 'Pending' Status", HttpStatus.NOT_FOUND));
+
+        OrderStatus currentStatus = shopOrder.getOrderStatus();
+        if (!currentStatus.getName().equals(StatusName.PENDING.name()) && shopOrder.getPaymentId() == null) {
+            throw new AppException("Shop Order is already payed", HttpStatus.NOT_MODIFIED);
+        }
+
+        OrderStatus modifiedStatus = orderStatusRepository.findByNameIgnoreCase(StatusName.PAYMENT_FULFILLED.name());
+        shopOrder.setOrderStatus(modifiedStatus);
+        shopOrder.setPaymentId(paymentRequest.getPaymentId());
+
+        return shopOrderRepository.save(shopOrder);
+    }
+
+    public Boolean hasUserPendingShopOrder(Long userId) {
+        var user = userRepository.findById(userId)
+                .orElseThrow(() -> new AppException("User is not found with matching id", HttpStatus.NOT_FOUND));
+
+        var shopOrder = shopOrderRepository.findByCustomerIdAndOrderStatusIgnoreCase(userId, StatusName.PENDING.name());
+        return shopOrder.isPresent();
+    }
+
+    public List<ShopOrder> getShopOrdersByUserId(Long userId) {
+        var user = userRepository.findById(userId)
+                .orElseThrow(() -> new AppException("User is not found with matching id", HttpStatus.NOT_FOUND));
+
+        return shopOrderRepository.findByCustomer(user)
+                .orElseThrow(() -> new AppException("No Order is found from User", HttpStatus.NOT_FOUND));
     }
 }
