f6813002b8ef8cfdc937f7fb2f8fcd8820eb9db1
Princz Mia <princz.mia@gmail.com>
2024-05-06 20:47:43 +0200

    Update UserReviewRepository
    Updated UserReviewRepository to include methods for retrieving reviews by ordered product and user. Changed the return types of findAllByOrderedProduct and findAllByUser methods to Optional<List<UserReview>>. Also added a new method findByUserAndOrderedProduct to retrieve a review by both user and ordered product.

--- backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/user_review/UserReviewRepository.java
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/user_review/UserReviewRepository.java
@@ -5,10 +5,12 @@ import com.princz_mia.viaubv18_coffee_shop.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserReviewRepository extends JpaRepository<UserReview, Long> {
-    Optional<UserReview> findByOrderedProduct(Product product);
-    Optional<UserReview> findByUser(User user);
+    Optional<List<UserReview>> findAllByOrderedProduct(Product product);
+    Optional<List<UserReview>> findAllByUser(User user);
+    Optional<UserReview> findByUserAndOrderedProduct(User userById, Product productById);
 }
