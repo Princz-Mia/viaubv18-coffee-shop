af40ee1b92309221f860d439f5dcd2284b7710ab
Princz Mia <princz.mia@gmail.com>
2024-05-06 20:45:24 +0200

    Add user review endpoints
    Created a new UserReviewController class with endpoints for managing product reviews. Added endpoints for retrieving reviews by productId and adding a new product review. These endpoints interact with UserReviewService to perform the necessary operations.

--- backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/user_review/UserReviewController.java
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/user_review/UserReviewController.java
@@ -1,13 +1,30 @@
 package com.princz_mia.viaubv18_coffee_shop.user_review;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/review")
+@RequestMapping("/api/v1/productReviews")
 @RequiredArgsConstructor
 @Slf4j
 public class UserReviewController {
+
+    private final UserReviewService userReviewService;
+
+    @GetMapping()
+    public ResponseEntity<List<UserReview>> getReviewsByProductId(@RequestParam(value = "productId", required = true) Long productId) {
+        List<UserReview> userReviews = userReviewService.getReviewsByProductId(productId);
+        return ResponseEntity.ok(userReviews);
+    }
+
+    @PostMapping(path = "/add")
+    public ResponseEntity<UserReview> addProductReview(@RequestBody @Valid UserReviewRequest userReviewRequest) {
+        UserReview userReview = userReviewService.addProductReview(userReviewRequest);
+        return ResponseEntity.ok(userReview);
+    }
 }
