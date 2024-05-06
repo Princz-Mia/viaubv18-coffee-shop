c56ac6dc7943ae7190326526e8062c3df4e254b1
Princz Mia <princz.mia@gmail.com>
2024-05-06 20:37:54 +0200

    Add user address association
    Extended AddressService to include a new method `setAddressToUser` for associating an address with a user. This method accepts a userId and an AddressRequest, retrieves the user and country details, and either updates the existing address association or creates a new address entry. It then updates the user's address association accordingly.

--- backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/address/AddressService.java
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/address/AddressService.java
@@ -2,6 +2,7 @@ package com.princz_mia.viaubv18_coffee_shop.address;
 
 import com.princz_mia.viaubv18_coffee_shop.country.CountryRepository;
 import com.princz_mia.viaubv18_coffee_shop.exception.AppException;
+import com.princz_mia.viaubv18_coffee_shop.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -14,6 +15,7 @@ public class AddressService {
 
     private final AddressRepository addressRepository;
     private final CountryRepository countryRepository;
+    private final UserRepository userRepository;
 
     public Address createAddress(AddressRequest addressRequest) {
         var country = countryRepository.findByNameIgnoreCase(addressRequest.getCountryName())
@@ -45,5 +47,46 @@ public class AddressService {
                 .build();
 
         return addressRepository.save(address);
+    }
+
+    public Address setAddressToUser(Long userId, AddressRequest addressRequest) {
+        var country = countryRepository.findByNameIgnoreCase(addressRequest.getCountryName())
+                .orElseThrow(() -> new AppException("Country is not found with matching name", HttpStatus.NOT_FOUND));
+
+        var userById = userRepository.findById(userId)
+                .orElseThrow(() -> new AppException("User is not found with matching Id.", HttpStatus.NOT_FOUND));
+
+        var optionalAddress = addressRepository.findByPropertiesIgnoreCase(
+                country.getId(),
+                addressRequest.getRegion(),
+                addressRequest.getPostalCode(),
+                addressRequest.getCity(),
+                addressRequest.getAddressLine1(),
+                addressRequest.getAddressLine2(),
+                addressRequest.getStreetNumber(),
+                addressRequest.getUnitNumber()
+        );
+
+        if (optionalAddress.isPresent()) {
+            Address existingAddress = optionalAddress.get();
+            userById.setAddress(existingAddress);
+            userRepository.save(userById);
+            return existingAddress;
+        }
+
+        Address newAddress = Address.builder()
+                .country(country)
+                .region(addressRequest.getRegion())
+                .postalCode(addressRequest.getPostalCode())
+                .city(addressRequest.getCity())
+                .addressLine1(addressRequest.getAddressLine1())
+                .addressLine2(addressRequest.getAddressLine2())
+                .streetNumber(addressRequest.getStreetNumber())
+                .unitNumber(addressRequest.getUnitNumber())
+                .build();
+
+        userById.setAddress(newAddress);
+        userRepository.save(userById);
+        return addressRepository.save(newAddress);
     }
 }
