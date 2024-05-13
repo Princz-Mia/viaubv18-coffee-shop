d2cddfe1e48389a05a914a9d5202104b778c06d5
Princz Mia <princz.mia@gmail.com>
2024-05-13 17:22:30 +0200

    Expand ProductCategoryController to handle retrieving all categories
    - Added method to retrieve all product categories

--- backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/product/category/ProductCategoryController.java
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/product/category/ProductCategoryController.java
@@ -2,12 +2,24 @@ package com.princz_mia.viaubv18_coffee_shop.product.category;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/product/category")
 @RequiredArgsConstructor
 @Slf4j
 public class ProductCategoryController {
+
+    private final ProductCategoryService productCategoryService;
+
+    @GetMapping(path = "/getAllCategory")
+    public ResponseEntity<List<ProductCategory>> getAllCategory() {
+        List<ProductCategory> categories = productCategoryService.getAllCategory();
+        return ResponseEntity.ok(categories);
+    }
 }
