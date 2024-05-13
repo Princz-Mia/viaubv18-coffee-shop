279ef5d891640b8c597702f7f5d65d2250d2e6e5
Princz Mia <princz.mia@gmail.com>
2024-05-13 17:23:06 +0200

    Implement getAllCategory method in ProductCategoryService
    - Added method to retrieve all product categories from the repository

--- backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/product/category/ProductCategoryService.java
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/product/category/ProductCategoryService.java
@@ -1,9 +1,19 @@
 package com.princz_mia.viaubv18_coffee_shop.product.category;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 @Slf4j
 public class ProductCategoryService {
+
+    private final ProductCategoryRepository productCategoryRepository;
+
+    public List<ProductCategory> getAllCategory() {
+        return productCategoryRepository.findAll();
+    }
 }
