cfd5780858cc5b5a3b449831234c75029ada4fdd
Princz Mia <princz.mia@gmail.com>
2024-05-27 22:06:24 +0200

    Update ShopOrderRepository for ShopOrderStatus and renamed query methods
    - Changed findByOrderStatus to findByShopOrderStatus.
    - Changed findByCustomerIdAndOrderStatusIgnoreCase to findByCustomerIdAndShopOrderStatusNameIgnoreCase.

--- backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/shop_order/ShopOrderRepository.java
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/shop_order/ShopOrderRepository.java
@@ -1,7 +1,7 @@
-package com.princz_mia.viaubv18_coffee_shop.order;
+package com.princz_mia.viaubv18_coffee_shop.shop_order;
 
 import com.princz_mia.viaubv18_coffee_shop.address.Address;
-import com.princz_mia.viaubv18_coffee_shop.order.status.OrderStatus;
+import com.princz_mia.viaubv18_coffee_shop.shop_order.status.ShopOrderStatus;
 import com.princz_mia.viaubv18_coffee_shop.shipping_method.ShippingMethod;
 import com.princz_mia.viaubv18_coffee_shop.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -17,9 +17,9 @@ public interface ShopOrderRepository extends JpaRepository<ShopOrder, Long> {
     Optional<List<ShopOrder>> findByCustomer(User user);
     Optional<List<ShopOrder>> findByShippingAddress(Address address);
     Optional<List<ShopOrder>> findByShippingMethod(ShippingMethod shippingMethod);
-    Optional<List<ShopOrder>> findByOrderStatus(OrderStatus orderStatus);
+    Optional<List<ShopOrder>> findByShopOrderStatus(ShopOrderStatus shopOrderStatus);
     Optional<List<ShopOrder>> findByOrderDate(Timestamp orderDate);
 
-    @Query("SELECT s FROM ShopOrder s WHERE s.customer.id = ?1 and LOWER(s.orderStatus.name) = LOWER(?2)")
-    Optional<ShopOrder> findByCustomerIdAndOrderStatusIgnoreCase(Long userId, String name);
+    @Query("SELECT s FROM ShopOrder s WHERE s.customer.id = ?1 and LOWER(s.shopOrderStatus.name) = LOWER(?2)")
+    Optional<ShopOrder> findByCustomerIdAndShopOrderStatusNameIgnoreCase(Long userId, String name);
 }
