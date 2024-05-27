812d640433778c784dc8706b1281c2df119c791f
Princz Mia <princz.mia@gmail.com>
2024-05-27 22:29:45 +0200

    Refactor OrderItemController to ShopOrderItemController
    - Renamed OrderItemController class to ShopOrderItemController to maintain consistency with the related service and entity names
    - Updated method signatures and parameter names accordingly to reflect the renaming

---
+++ backend/src/main/java/com/princz_mia/viaubv18_coffee_shop/shop_order/item/ShopOrderItemController.java
@@ -0,0 +1,30 @@
+package com.princz_mia.viaubv18_coffee_shop.shop_order.item;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/shopOrderItem")
+@RequiredArgsConstructor
+@Slf4j
+public class ShopOrderItemController {
+
+    private final ShopOrderItemService shopOrderItemService;
+
+    @PostMapping(path = "/create")
+    public ResponseEntity<ShopOrderItem> createOrderItem(@RequestBody @Valid ShopOrderItemRequest shopOrderItemRequest) {
+        ShopOrderItem shopOrderItem = shopOrderItemService.createOrderItem(shopOrderItemRequest);
+        return ResponseEntity.ok(shopOrderItem);
+    }
+
+    @GetMapping(path = "/getByShopOrderId/{shopOrderId}")
+    public ResponseEntity<List<ShopOrderItem>> getShopOrderItemsByShopOrderId(@PathVariable Long shopOrderId) {
+        List<ShopOrderItem> shopOrderItems = shopOrderItemService.getShopOrderItemsByShopOrderId(shopOrderId);
+        return ResponseEntity.ok(shopOrderItems);
+    }
+}
